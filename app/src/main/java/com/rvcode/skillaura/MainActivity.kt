3c8970d6ca6aa98cc6ec15f8b4d0c3536b723c3d
Ramveer <ramveerk7802@gmail.com>
2025-08-07 15:04:16 +0530

    Initial project setup also setup important dependency like hilt,navigation, retrofit

---
+++ app/src/main/java/com/rvcode/skillaura/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.rvcode.skillaura
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rvcode.skillaura.ui.theme.SkillAuraTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SkillAuraTheme {
+                Box (
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ){
+                    Greeting("Hi Ramveer, Android app developer\n Noida Up", modifier = Modifier.padding(10.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
---
+++ app/src/main/java/com/rvcode/skillaura/MainApplication.kt
@@ -0,0 +1,9 @@
+package com.rvcode.skillaura
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class MainApplication : Application()  {
+}
